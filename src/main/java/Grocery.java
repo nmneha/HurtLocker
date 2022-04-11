6867e196a7211153faf1a6bb4aab97a7b81a2adc
nusera <nusera.neha@gmail.com>
2022-04-11 14:23:53 -0400

    test test test

---
+++ src/main/java/Grocery.java
@@ -0,0 +1,13 @@
+public class Grocery {
+    String name;
+    double price;
+    String type;
+    String expiration;
+
+    public Grocery(String name, double price, String type, String expiration) {
+        this.name = name;
+        this.price = price;
+        this.type = type;
+        this.expiration = expiration;
+    }
+}
---
+++ src/main/java/JerkSON.java
@@ -0,0 +1,26 @@
+import org.apache.commons.io.IOUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JerkSON {
+    public List<String> groceryList = new ArrayList<>();
+
+    public String readRawDataToString() throws Exception{
+        ClassLoader classLoader = getClass().getClassLoader();
+        String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
+        return result;
+    }
+
+
+    public List<String> splitRawData(String raw) {
+        Pattern p = Pattern.compile("name.*?[##$]", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = p.matcher(raw);
+        while(matcher.find()) {;
+            groceryList.add(matcher.group());
+        }
+        return groceryList;
+    }
+}
